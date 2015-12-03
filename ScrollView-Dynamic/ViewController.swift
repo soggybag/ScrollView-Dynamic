1c31ff80f646fb10e4fa1d7a395a9279c8eb57a2
Mitchell Hudson <soggybag@gmail.com>
2015-12-02 17:20:53 -0800

    Init

--- ScrollView-Dynamic/ViewController.swift
+++ ScrollView-Dynamic/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //let container = UIView()
+        var array = [UIView]()
+        
+        var widthOfScrollView:CGFloat = 0.0
+        
+        for var i in 0...10 {
+            
+            widthOfScrollView += 100
+            print(widthOfScrollView)
+            
+            // let box = UIView(frame: CGRect(x: 110 * i, y: 0, width: 100, height: 100))
+            let box = UIImageView(frame: CGRect(x: 110 * i, y: 0, width: 100, height: 100))
+            box.image = UIImage(named: "alien")
+            
+            //box.contentMode = .Center
+            box.contentMode = .ScaleAspectFill
+            
+            array.append(box)
+            let hue = 360 / 10 * CGFloat(i) / 360
+            //print(hue)
+            box.backgroundColor = UIColor(hue: hue, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+            // container.addSubview(box)
+            scrollView.addSubview(box)
+            // self.view.addSubview(box)
+        }
+        
+        scrollView.contentSize = CGSize(width: widthOfScrollView + 100, height: 100)
+        // scrollView.addSubview(container)
     }
 
     override func didReceiveMemoryWarning() {
